3a81d2b468ecc7beefd485f8002610c0a28e5f2b
yukuda <leeyookoo@naver.com>
2021-07-15 13:08:43 +0900

    CircleArea, Math000

---
+++ 2019_04_18/src/Math000/Math000.java
@@ -0,0 +1,17 @@
+package Math000;
+
+public class Math000 {
+	   int temp=999;
+	   public int getTemp() {
+		   return temp;
+	   }
+	   
+	   public int max(int a, int b) {
+	      if(a > b ) {
+	         return a;
+	      }
+	      else {
+	         return b;
+	      }
+	   }
+}
